d33e305e2afd956171c6fba02dc1d689f7e4712e
ewan <ewan@MacBook-Pro-ewan.local>
2024-05-08 00:48:06 +0300

    some key true/false while building compose

--- settings.py
+++ settings.py
@@ -7,9 +7,15 @@ root_dir = pathlib.Path(__file__).parent.resolve()
 # DOTENV FILE
 config = dotenv_values(f"{root_dir}/.env")
 
+# When it's building it feeds from .env  docker url
+if config['POSTGRES_DOCKER_BUILD'] == 'True':
+    PG_HOST = config["POSTGRES_HOST_DOCKER"]
+elif config['POSTGRES_DOCKER_BUILD'] == 'False':
+    PG_HOST = config["POSTGRES_HOST_LOCAL"]
+
+
 # PARAMS FOR THE DATABASE
 PG_USER = config["POSTGRES_USER"]
 PG_PASS = config["POSTGRES_PASSWORD"]
-PG_HOST = config["POSTGRES_HOST"]
 PG_PORT = config["POSTGRES_PORT"]
 PG_DB_NAME = config["POSTGRES_DB_NAME"]
